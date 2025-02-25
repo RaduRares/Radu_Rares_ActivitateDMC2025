769afa064c5412706c0ad89c66d942c69b6e1477
RaduRares <andrei.radu@mta.ro>
2025-02-25 11:29:59 +0200

    Lab2 commit

--- app/src/main/java/com/example/app_bun/MainActivity.java
+++ app/src/main/java/com/example/app_bun/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.app_bun;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void FunctieModificareText(View view) {
+
+        Button buton=findViewById(R.id.button);
+        TextView text=findViewById(R.id.textView);
+        text.setText("Modificare View");
+        buton.setText("s-a modificat si textul");
     }
 }
